e7924452de595ff04ba28b2d0c274f4a58ae5cb6
Anthony Sennett <anthony.sennett@gmail.com>
2014-12-28 15:30:31 -0600

    ground body generators; basic ground data

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,14 @@
-define(['environment/world'], function(World){
+define([
+	'environment/world',
+	'environment/groundData',
+	'environment/groundBodyGenerator',
+	'environment/groundBodyConstructor',
+], function(World, groundData, GroundBodyGenerator, groundBodyConstructor){
 	var App = function(){};
 	App.prototype.run = function(){
-		var world = new World();
+		var world = new World(new GroundBodyGenerator(groundBodyConstructor));
 		world.setCar();
+		world.setGround(groundData);
 		world.start();
 	};
 	return App;
---
+++ scripts/environment/groundData.js
@@ -0,0 +1,12 @@
+define(function(){
+	// defines the ground.  a collection of points to be joined by immovable straight lines
+	return {
+		points: [{
+			x: 200,
+			y: 150
+		},{
+			x: 300,
+			y: 250
+		}]
+	};
+});
