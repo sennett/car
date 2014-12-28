7ff22d2828fa7123b8765b25cbd3800c3187b01f
Anthony Sennett <anthony.sennett@gmail.com>
2014-12-28 05:54:24 -0600

    adding hello world dependency injection

--- scripts/environment/groundBodyGenerator.js
+++ scripts/environment/groundBodyGenerator.js
@@ -1,8 +1,12 @@
 define(function(){
-	// takes two coordinates and generates a physics body from them
-	return {
-		generate: function(point1, point2){
-			
+	// takes two coordinates and generates numbers from a
+	return function(groundBodyConstructor) {
+		var _this = this;
+
+		this.groundBodyConstructor = groundBodyConstructor;
+
+		this.generate = function(point1, point2){
+			return _this.groundBodyConstructor.construct();
 		}
 	}
 });
--- tests/environment/groundBodyGeneratorTest.js
+++ tests/environment/groundBodyGeneratorTest.js
@@ -1,7 +1,16 @@
 define(['scripts/environment/groundBodyGenerator'], function(groundBodyGenerator) {
-	describe("A suite", function () {
-		it("contains spec with an expectation", function () {
-			expect(true).toBe(true);
+
+	beforeEach(function(){
+		this.constructorMock = jasmine.createSpyObj('bodyConstructor', ['construct']);
+		this.groundBodyGenerator = new groundBodyGenerator(this.constructorMock);
+	});
+
+	describe("groundBodyGenerator", function () {
+		describe('generate', function(){
+			it("should call constructor", function () {
+				this.groundBodyGenerator.generate();
+				expect(this.constructorMock.construct).toHaveBeenCalled();
+			});
 		});
 	});
 });
