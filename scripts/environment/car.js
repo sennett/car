5cf7fe25660d9e6e5ac76ce65a621eaf3146d6db
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-11 17:37:30 +0100

    wheel on the car

--- scripts/environment/car.js
+++ scripts/environment/car.js
@@ -4,6 +4,7 @@ define(['box2dweb'], function(Box2D){
 	var b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
 	var b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
 	var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+	var b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef;
 
 	var createCarBody = function(world){
 		var bodyDef = new b2BodyDef();
@@ -17,15 +18,34 @@ define(['box2dweb'], function(Box2D){
 		fixtureDef.friction = 0.3;
 		fixtureDef.restitution = 0.2;
 
-		world.CreateBody(bodyDef).CreateFixture(fixtureDef);
+		var carBody = world.CreateBody(bodyDef);
+		carBody.CreateFixture(fixtureDef);
+		return carBody;
 	};
 
 	var createFrontWheel = function(world, carBody){
+		var bodyDef = new b2BodyDef();
+		bodyDef.type = b2Body.b2_dynamicBody;
+		bodyDef.position.Set(5, 5);
 
+		var fixtureDef = new b2FixtureDef();
+		fixtureDef.shape = new b2CircleShape(1);
+		fixtureDef.density = 1.0;
+		fixtureDef.friction = 0.3;
+		fixtureDef.restitution = 0.2;
+
+		var wheel = world.CreateBody(bodyDef);
+		wheel.CreateFixture(fixtureDef);
+
+		var axleDef = new b2RevoluteJointDef();
+		axleDef.Initialize(carBody, wheel, wheel.GetWorldCenter());
+		axleDef.enableMotor = true;
+		axleDef.motorSpeed = 5;
+		axleDef.maxMotorTorque = 10;
+		world.CreateJoint(axleDef);
 	};
 
 	var createBackWheel = function(world, carBody){
-
 	};
 
 	return {
