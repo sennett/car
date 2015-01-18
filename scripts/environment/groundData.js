cdcb82c85f15ad45884f0f3b3165e239425c9278
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-18 11:23:09 +0100

    more ground

--- scripts/environment/groundData.js
+++ scripts/environment/groundData.js
@@ -1,7 +1,7 @@
 define(function(){
 	var groundData = [{x:2, y:16}, {x:8, y:16}];
 
-	for (var i = groundData.length; i < 100; i++) {
+	for (var i = groundData.length; i < 1000; i++) {
 		var previousPoint = groundData[i - 1];
 		var newPoint = {x: previousPoint.x + 2, y: Math.random() * 3 - 1.5 + previousPoint.y};
 		groundData.push(newPoint);
