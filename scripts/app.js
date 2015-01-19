d07863279aea1dff28fe50368817fc30fb84798c
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-19 21:59:36 +0100

    refactoring simulation kick off

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,16 @@
 define([
-	'environment/simulation',
-	'environment/groundData',
-	'environment/provider/groundSegmentProvider',
-	'environment/provider/physicsWorldProvider',
-	'environment/car',
-	'environment/renderer',
-	'environment/ticker',
-	'environment/ground',
-	'environment/endStateDetector'
-], function(Simulation, groundData, GroundSegmentProvider, physicsWorldProvider, car, Renderer, ticker, Ground, EndStateDetector){
+	'environment/simulationBuilder',
+	'environment/car'
+], function(simulationBuilder, car){
 	var App = function(){};
 	App.prototype.run = function(){
-		var ground = new Ground(new GroundSegmentProvider());
-		ground.setData(groundData);
-		var simulation = new Simulation(physicsWorldProvider, new Renderer, ticker, new EndStateDetector());
-		simulation.initialise(ground);
+		var simulation = simulationBuilder.createSimulation();
+
 		simulation.onStop(function(score){
 			console.log('stopped: travelled ' + score + 'm');
 			simulation.start(car);
 		});
+
 		simulation.start(car);
 	};
 	return App;
---
+++ scripts/environment/simulationBuilder.js
@@ -0,0 +1,20 @@
+define([
+	'environment/simulation',
+	'environment/groundData',
+	'environment/provider/groundSegmentProvider',
+	'environment/provider/physicsWorldProvider',
+	'environment/renderer',
+	'environment/ticker',
+	'environment/ground',
+	'environment/endStateDetector'
+], function(Simulation, groundData, GroundSegmentProvider, physicsWorldProvider, Renderer, ticker, Ground, EndStateDetector) {
+	return {
+		createSimulation:function () {
+			var ground = new Ground(new GroundSegmentProvider());
+			ground.setData(groundData);
+			var simulation = new Simulation(physicsWorldProvider, new Renderer, ticker, new EndStateDetector());
+			simulation.initialise(ground);
+			return simulation;
+		}
+	};
+});
