f3c06b98dd0a89ec4376fdc665c8553d95e5fce5
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-29 22:06:56 +0100

    started making genome

---
+++ scripts/evolution/Genome.js
@@ -0,0 +1,20 @@
+define(function () {
+	return {
+		angle1: undefined,
+		magnitude1: undefined,
+		angle2: undefined,
+		magnitude2: undefined,
+		angle3: undefined,
+		magnitude3: undefined,
+		angle4: undefined,
+		magnitude4: undefined,
+		angle5: undefined,
+		magnitude1: undefined,
+		angle1: undefined,
+		magnitude1: undefined,
+		angle1: undefined,
+		magnitude1: undefined,
+		angle1: undefined,
+		magnitude1: undefined,
+	};
+});
