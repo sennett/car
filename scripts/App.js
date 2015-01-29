8857be974da04cce6140058421a5f7b44992acdd
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-29 14:42:02 +0100

    moving app to prototype

--- scripts/App.js
+++ scripts/App.js
@@ -1,12 +1,19 @@
 define([], function(){
-	return function(simulation, evolutionEngine){
-		this.run = function(){
-			simulation.onStop(function(score){
-				evolutionEngine.registerScore(score);
-				simulation.start(evolutionEngine.nextCar());
-			});
-
-			simulation.start(evolutionEngine.nextCar());
-		};
+	var App = function(simulation, evolutionEngine){
+		this.simulation = simulation;
+		this.evolutionEngine = evolutionEngine;
+		debugger;
 	};
+
+	App.prototype = {
+		run: function() {
+			this.simulation.onStop(function (score) {
+				this.evolutionEngine.registerScore(score);
+				this.simulation.start(evolutionEngine.nextCar());
+			});
+			this.simulation.start(evolutionEngine.nextCar());
+		}
+	};
+
+	return App;
 });
