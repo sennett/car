2b5ac7ff89a589105475e78228dc0a25b7d82b6e
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-29 21:10:57 +0100

    production build

---
+++ scripts/buildConfig.js
@@ -0,0 +1,15 @@
+({
+	"name": "buildWrapper",
+	"baseUrl": "./",
+	"out": "../build/main.js",
+
+	mainConfigFile: 'main.js',
+
+	paths: {
+		'wire/builder/rjs': '../node_modules/wire-rjs-builder/builder'
+	},
+
+	optimize: "uglify",
+	removeCombined: true
+
+})
