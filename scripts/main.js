d8b6f9056e27596d40bc584ff47c771926a36422
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-29 14:25:13 +0100

    moving to requirejs, adding bulk of wire.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,21 @@
-(function(curl) {
+// requirejs base config
+require.config({
+	baseUrl: "scripts",
+	packages: [
+		{ name: 'wire', location: '../bower_components/wire', main: 'wire' },
+		{ name: 'when', location: '../bower_components/when', main: 'when' },
+		{ name: 'meld', location: '../bower_components/meld', main: 'meld' }
+	],
+	paths:{
+		box2dweb: 'vendor/Box2dWeb-2.1.a.3'
+	},
+	shim: {
+		box2dweb: {
+			exports: 'Box2D'
+		}
+	}
+});
 
-	var config = {
-		packages: [
-			{ name: 'curl', location: 'bower_components/curl/src/curl', main: 'curl' },
-			{ name: 'wire', location: 'bower_components/wire', main: 'wire' },
-			{ name: 'when', location: 'bower_components/when', main: 'when' },
-			{ name: 'meld', location: 'bower_components/meld', main: 'meld' },
-			{ name: 'poly', location: 'bower_components/poly', main: 'poly' }
-		]
-	};
-
-	curl(config, ['wire!app/wireSpec']);
-
-})(curl);
+define(['wire!wireSpec'], function () {
+	return {};
+});
--- scripts/wireSpec.js
+++ scripts/wireSpec.js
@@ -1,7 +1,42 @@
 define({
 	app: {
 		create: {
-			module: 'App'
+			module: 'App',
+			args: [{ $ref: "simulation"}, { $ref: "evolutionEngine"}]
+		},
+		ready: "run"
+	},
+
+	simulation: {
+		create:{
+			module: 'environment/Simulation',
+			args: [{ $ref: "physicsWorldProvider"}, { $ref: "renderer"}, { $ref: "ticker" }, { $ref: "endStateDetector" }]
+		}
+	},
+	physicsWorldProvider: {
+		create: {
+			module: 'environment/provider/PhysicsWorldProvider'
+		}
+	},
+	renderer: {
+		create: {
+			module:'environment/Renderer'
+		}
+	},
+	ticker:{
+		create: {
+			module: 'environment/Ticker'
+		}
+	},
+	endStateDetector: {
+		create: {
+			module: 'environment/EndStateDetector'
+		}
+	},
+
+	evolutionEngine: {
+		create: {
+			module: 'evolution/Engine'
 		}
 	}
 });
