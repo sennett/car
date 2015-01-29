32a5696e98dd01f5918bc0af56cdf7c08106e342
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-29 22:09:20 +0100

    added rest of genome

--- scripts/evolution/Genome.js
+++ scripts/evolution/Genome.js
@@ -9,12 +9,14 @@ define(function () {
 		angle4: undefined,
 		magnitude4: undefined,
 		angle5: undefined,
-		magnitude1: undefined,
-		angle1: undefined,
-		magnitude1: undefined,
-		angle1: undefined,
-		magnitude1: undefined,
-		angle1: undefined,
-		magnitude1: undefined,
+		magnitude5: undefined,
+		angle6: undefined,
+		magnitude6: undefined,
+		angle7: undefined,
+		magnitude7: undefined,
+		angle8: undefined,
+		magnitude8: undefined,
+		wheelRadius1: undefined,
+		wheelRadius2: undefined
 	};
 });
