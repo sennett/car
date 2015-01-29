91715184ce9cb1e7f43dbf02afa624f905dedf07
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-29 14:51:32 +0100

    moving physics world provider to prototype

--- scripts/environment/provider/PhysicsWorldProvider.js
+++ scripts/environment/provider/PhysicsWorldProvider.js
@@ -1,11 +1,15 @@
 define(['box2dweb'], function(Box2D) {
-	return function() {
-		this.getWorld = function(){
+	var PhysicsWorldProvider = function() {};
+
+	PhysicsWorldProvider.prototype = {
+		getWorld: function(){
 			// http://stackoverflow.com/a/1608546/614523
 			return new Box2D.Dynamics.b2World(
 				new Box2D.Common.Math.b2Vec2(0, 9.8),
 				true
-			)
+			);
 		}
 	};
+
+	return PhysicsWorldProvider;
 });
