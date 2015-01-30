f4f882108428f65b28cd61f2b44c4160aa4d5393
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-30 22:31:00 +0100

    getting actual genome

--- scripts/evolution/Engine.js
+++ scripts/evolution/Engine.js
@@ -1,44 +1,10 @@
-define(['../core/car'], function (car) {
+define(['evolution/Genome'], function (Genome) {
 	var Engine = function(){
 	};
 
 	Engine.prototype = {
 		nextGenome: function(){
-			var createAngle = function(x){
-				return Math.PI * x / 4;
-			};
-			return {
-				angle0: createAngle(0),
-				magnitude0: 1,
-				angle1: createAngle(1),
-				magnitude1: 2,
-				angle2: createAngle(2),
-				magnitude2: 1,
-				angle3: createAngle(3),
-				magnitude3: 2,
-				angle4: createAngle(4),
-				magnitude4: 1,
-				angle5: createAngle(5),
-				magnitude5: 2,
-				angle6: createAngle(6),
-				magnitude6: 1,
-				angle7: createAngle(7),
-				magnitude7: 2,
-
-				wheelVertex0: undefined,
-				wheelRadius0: undefined,
-				wheelVertex1: undefined,
-				wheelRadius1: undefined,
-
-				runForVertices: function(runForVertex){
-					for (var i = 0; i <=7; i++)
-						runForVertex(this['angle' + i], this['magnitude' + i]);
-				},
-
-				loopWheels: function(runForWheel){
-
-				}
-			};
+			return Genome;
 		},
 		registerScore: function(score){
 			console.log('stopped: travelled ' + score + 'm');
--- scripts/evolution/Genome.js
+++ scripts/evolution/Genome.js
@@ -1,26 +1,29 @@
 define(function () {
+	var createAngle = function(x){
+		return Math.PI * x / 4;
+	};
 	return {
-		angle0: undefined,
-		magnitude0: undefined,
-		angle1: undefined,
-		magnitude1: undefined,
-		angle2: undefined,
-		magnitude2: undefined,
-		angle3: undefined,
-		magnitude3: undefined,
-		angle4: undefined,
-		magnitude4: undefined,
-		angle5: undefined,
-		magnitude5: undefined,
-		angle6: undefined,
-		magnitude6: undefined,
-		angle7: undefined,
-		magnitude7: undefined,
+		angle0: createAngle(0),
+		magnitude0: 1,
+		angle1: createAngle(1),
+		magnitude1: 2,
+		angle2: createAngle(2),
+		magnitude2: 1,
+		angle3: createAngle(3),
+		magnitude3: 2,
+		angle4: createAngle(4),
+		magnitude4: 1,
+		angle5: createAngle(5),
+		magnitude5: 2,
+		angle6: createAngle(6),
+		magnitude6: 1,
+		angle7: createAngle(7),
+		magnitude7: 2,
 
-		wheelVertex0: undefined,
-		wheelRadius0: undefined,
-		wheelVertex1: undefined,
-		wheelRadius1: undefined,
+		wheelVertex0: 1,
+		wheelRadius0: 1,
+		wheelVertex1: 2,
+		wheelRadius1: 1,
 
 		runForVertices: function(runForVertex){
 			for (var i = 0; i <=7; i++)
