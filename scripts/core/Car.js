2a4d0cce08a69d0212bb2f8c36699fb10d5861d4
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-30 23:04:52 +0100

    creating wheels

--- scripts/core/Car.js
+++ scripts/core/Car.js
@@ -7,10 +7,6 @@ define(['box2dweb', 'underscore'], function(Box2D, _){
 	var b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef;
 	var b2Vec2 = Box2D.Common.Math.b2Vec2;
 
-	var Car = function(genome){
-		this.genome = genome;
-	};
-
 	var createSubBodyFixtureDef = function(point1, point2){
 		var points = [new b2Vec2(0,0), point1, point2];
 
@@ -25,13 +21,15 @@ define(['box2dweb', 'underscore'], function(Box2D, _){
 	};
 
 	var createBody = function(){
-		var twoDVertices = [];
-		this.genome.forEachVertex(function(angle, magnitude, id){
-			twoDVertices.push({
+		var createVertex = _.bind(function(angle, magnitude, id){
+			this.twoDVertices.push({
 				id: id,
 				location: new b2Vec2(Math.cos(angle) * magnitude, Math.sin(angle) * magnitude)
 			});
-		});
+		}, this);
+
+		this.twoDVertices = [];
+		this.genome.forEachVertex(createVertex);
 
 		var bodyDef = new b2BodyDef();
 		bodyDef.type = b2Body.b2_dynamicBody;
@@ -39,24 +37,61 @@ define(['box2dweb', 'underscore'], function(Box2D, _){
 		var carBody = this.world.CreateBody(bodyDef);
 
 		// create body shape using multiple fixtures (box2d does not support convex polygons)
-		for (var i = 0; i < twoDVertices.length - 1; i++)
-			carBody.CreateFixture(createSubBodyFixtureDef.call(this, twoDVertices[i].location, twoDVertices[i + 1].location));
-		carBody.CreateFixture(createSubBodyFixtureDef.call(this, _.last(twoDVertices).location, _.first(twoDVertices).location));
+		for (var i = 0; i < this.twoDVertices.length - 1; i++)
+			carBody.CreateFixture(createSubBodyFixtureDef.call(this, this.twoDVertices[i].location, this.twoDVertices[i + 1].location));
+		carBody.CreateFixture(createSubBodyFixtureDef.call(this, _.last(this.twoDVertices).location, _.first(this.twoDVertices).location));
 
 		return carBody;
 	};
 
 	var createWheels = function(){
-		this.genome.forEachWheel(function(vertex, radius){
 
-		});
+		var createWheel = _.bind(function(vertexId, radius){
+			var vertex = _.findWhere(this.twoDVertices, { id: vertexId});
+			if (!vertex)
+				throw "invalid vertex for wheel from genome: " + vertexId;
+
+			var bodyDef = new b2BodyDef();
+			bodyDef.type = b2Body.b2_dynamicBody;
+
+			// copy vertex location, move to car body, and apply to wheel
+			var vertexLocationCopy = vertex.location.Copy();
+			vertexLocationCopy.Add(this.body.GetWorldCenter());
+			bodyDef.position = vertexLocationCopy;
+
+			var fixtureDef = new b2FixtureDef();
+			fixtureDef.shape = new b2CircleShape(radius);
+			fixtureDef.density = 1.0;
+			fixtureDef.friction = 1;
+			fixtureDef.restitution = 0.2;
+
+			var wheel = this.world.CreateBody(bodyDef);
+			wheel.CreateFixture(fixtureDef);
+
+			var axleDef = new b2RevoluteJointDef();
+			axleDef.Initialize(this.body, wheel, wheel.GetWorldCenter());
+			axleDef.enableMotor = true;
+			axleDef.motorSpeed = 5;
+			axleDef.maxMotorTorque = 10;
+			this.world.CreateJoint(axleDef);
+
+			this.wheels.push(wheel);
+		}, this);
+
+		this.wheels = [];
+		this.genome.forEachWheel(createWheel);
+	};
+
+	var Car = function(genome){
+		this.genome = genome;
 	};
 
 	Car.prototype = {
 		destroyPhysicsBodies: function(){
 			this.world.DestroyBody(this.body);
-			//this.world.DestroyBody(this.frontWheel);
-			//this.world.DestroyBody(this.backWheel);
+			_.each(this.wheels, function(wheel){
+				this.world.DestroyBody(wheel);
+			}, this);
 		},
 		initialisePhysicsBodies: function(world){
 			this.world = world;
