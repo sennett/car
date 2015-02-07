0ea7e2cdb51271d4bb26d67284db571a260f9ebf
Anthony Sennett <anthony.sennett@gmail.com>
2015-02-07 23:37:56 +0100

    ticker does not tick over itself

--- scripts/environment/Ticker.js
+++ scripts/environment/Ticker.js
@@ -1,11 +1,16 @@
-define(function () {
+define(['underscore'], function (_) {
 	var Ticker = function(){};
 
 	Ticker.prototype = {
+		running: false,
 		run: function(tick, simulation){
-			this.intervalId = window.setInterval(function(){
-				tick.call(simulation);
-			}, 1000 / 600);
+			this.intervalId = window.setInterval(_.bind(function(){
+				if (!this.running){
+					this.running = true;
+					tick.call(simulation);
+					this.running = false;
+				}
+			}, this), 1000 / 600);
 		},
 		stop: function(){
 			window.clearInterval(this.intervalId);
