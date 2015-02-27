92889d0459a2132b0ed06362fe3b40116d661c74
Anthony Sennett <anthony.sennett@gmail.com>
2015-02-27 21:22:14 +0100

    fix bug with ticker detection in end state detector

--- scripts/environment/EndStateDetector.js
+++ scripts/environment/EndStateDetector.js
@@ -8,21 +8,26 @@ define(function () {
 		initialise: function(carBody){
 			this.lastContact = Date.now().valueOf();
 			this.carBody = carBody;
+			this.resetTicks();
 		},
 
 		simulationEnded: function(){
 			this.ticks++;
 
-			return (
-				!this.carBody.IsAwake()
-					//|| this.ticks > this.tickTolerance
-					|| Date.now().valueOf() > this.lastContact + this.timeTolerance
-			);
+			var carBodySleeping = !this.carBody.IsAwake();
+			var ticksOverrun = this.ticks > this.tickTolerance;
+			var timeOverrun = Date.now().valueOf() > this.lastContact + this.timeTolerance;
+
+			return carBodySleeping || ticksOverrun || timeOverrun;
+		},
+
+		resetTicks: function(){
+			this.ticks = 0;
 		},
 
 		// b2ContactListener
 		BeginContact: function(){
-			this.ticks = 0;
+			this.resetTicks();
 			this.lastContact = Date.now().valueOf();
 		},
 		PreSolve: function(){},
