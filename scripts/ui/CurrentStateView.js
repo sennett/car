b3b1ebea0567bd335ec12424af109cfde18ee932
Anthony Sennett <anthony.sennett@gmail.com>
2015-03-07 09:06:05 +0100

    run button

--- scripts/ui/CurrentStateView.js
+++ scripts/ui/CurrentStateView.js
@@ -8,29 +8,36 @@ define([
 		this.onChangeSpeedRequest();
 	};
 
-    var CurrentStateView = function(currentScoreProvider, speedChanger){
+	var runClick = function(){
+		this.onRunRequest();
+	};
+
+	return function(currentScoreProvider, speedChanger, appRunner){
 		var view = new Ractive({
 			el: '#currentState',
 			template: template,
 			data: {
 				score: 0,
-				speed: 'fast forward'
+				speed: 'fast forward',
+				runDisabled: false
 			},
 			updateCurrentScore: _.throttle(function(score){
 				this.set('score', score >= 0 ? Math.round(score * 100) / 100 : 0);
 			}, 100),
-			onSpeedUp: function(){
+			onSimulationSpeedUp: function(){
 				this.set('speed', 'slow down');
 			},
-			onSlowDown: function(){
+			onSimulationSlowDown: function(){
 				this.set('speed', 'fast forward');
+			},
+			onSimulationRunning: function(){
+				this.set('runDisabled', true);
 			}
 		});
 
-		new CurrentStatePresenter(view, currentScoreProvider, speedChanger);
+		new CurrentStatePresenter(view, currentScoreProvider, speedChanger, appRunner);
 		view.on('changeSpeed', speedChangeClick.bind(view));
+		view.on('run', runClick.bind(view));
 		return view;
 	};
-    
-    return CurrentStateView;
 });
