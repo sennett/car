5d4e05d3c311b8d9f7efd5a1c89d76f60d9da963
Anthony Sennett <anthony.sennett@gmail.com>
2015-04-02 09:33:39 +0200

    using create prototype method to create ractive view

--- scripts/ui/GenerationView.js
+++ scripts/ui/GenerationView.js
@@ -3,44 +3,52 @@ define([
 	'rv!ui/templates/GenerationTemplate',
 	'ui/presenters/GenerationPresenter',
 	'ui/utils'], function(Ractive, template, GenerationPresenter, utils) {
-    var GenerationView = function(generationProvider){
-		var view = new Ractive({
+
+	function createPrototype() {
+		return new Ractive({
 			template: template,
 			append: true,
-			computed:{
-				percentComplete: function(){
+			computed: {
+				percentComplete: function () {
 					return this.get('lastMemberComplete') / this.get('generationSize') * 100;
 				},
-				progressBarUnitPercentage: function(){
+				progressBarUnitPercentage: function () {
 					return 1 / this.get('generationSize') * 100;
 				},
-				displayCurrentlyRunningMember: function(){
+				displayCurrentlyRunningMember: function () {
 					return this.get('lastMemberComplete') != this.get('generationSize');
 				}
 			},
-			setGenerationSize: function(generationSize){
+			setGenerationSize: function (generationSize) {
 				this.set('generationSize', generationSize);
 			},
-			runningMember: function(member){
+			runningMember: function (member) {
 				this.set('lastMemberComplete', member - 1);
 			},
-			memberComplete: function(member){
+			memberComplete: function (member) {
 				this.set('lastMemberComplete', member);
 			},
-			setGenerationNumber: function(generationNumber){
+			setGenerationNumber: function (generationNumber) {
 				this.set('generationNumber', generationNumber);
 			},
-			generationComplete: function(averageScore){
+			generationComplete: function (averageScore) {
 				this.set('generationComplete', true);
 				this.set('averageScore', utils.roundScore(averageScore));
 			},
-			newHighscore: function(highscore){
+			newHighscore: function (highscore) {
 				this.set('highscore', utils.roundScore(highscore));
 			}
 		});
+	};
+
+	var GenerationView = function(generationProvider){
+		var view = createPrototype();
 
 		new GenerationPresenter(view, generationProvider);
 		return view;
 	};
+	
+	GenerationView.prototype = _.extend(GenerationView.prototype, createPrototype());
+	
     return GenerationView;
 });
