f6447e30abec1f21b34458851a8b7b006ab3accb
Anthony Sennett <anthony.sennett@gmail.com>
2015-04-13 21:27:54 +0200

    little refactor

--- scripts/ui/GenerationsListView.js
+++ scripts/ui/GenerationsListView.js
@@ -3,15 +3,18 @@ define(['underscore', 'ui/GenerationView'], function(_, GenerationView) {
 		this.generations = [];
 	};
     
+	var checkGeneration = function(id){
+		if (!this.generations[id])
+			throw 'application exception: no generation with id ' + id + ' found';
+	};
+	
     GenerationsListView.prototype = _.extend(GenerationsListView.prototype, {
 		onNewGeneration:function(id){
 			this.generations[id] = new GenerationView();
 		},
 		onNewGenerationHighScore: function(id, highScore){
-			if (this.generations[id])
-				this.generations[id].newHighScore(highScore);
-			else
-				throw 'application exception: no generation with id ' + id + ' found';
+			checkGeneration.call(this, id);
+			this.generations[id].newHighScore(highScore);
 		},
 		onNewGenerationAverageScore: function(id, averageScore){
 			throw 'not implemented exception';
