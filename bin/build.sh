86fa4fc9d993bcb0c498e8570d9ddc6444c31b31
Anthony Sennett <anthony.sennett@gmail.com>
2015-06-01 10:45:12 +0200

    adding favicon

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,10 @@ rm -r build/*
 node node_modules/.bin/r.js -o scripts/buildConfig.js
 node node_modules/.bin/r.js -o scripts/wireBuildConfig.js
 
+# copy index file overwriting require config
 cat index.html |\
 	sed s,bower_components/requirejs/require.js,require.js,g |\
 	sed s,scripts/baseRequireConfig.js,productionRequireConfig.js,g \
 	> build/index.html
+	
+cp favicon.ico build
