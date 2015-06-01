d86e44d38e6974bdea07d8a64b5c9f0ea7d74a01
Anthony Sennett <anthony.sennett@gmail.com>
2015-06-01 09:29:56 +0200

    roulette selection deals with generation

--- scripts/evolution/selection/RouletteWheel.js
+++ scripts/evolution/selection/RouletteWheel.js
@@ -1,44 +1,48 @@
-define(['underscore', 'core/util', 'domain/genome'], function (_, util, genome) {
+define([
+	'underscore', 'core/util', 'domain/genome', 'core/appConfig', 'domain/generation'], 
+	function (_, util, genome, config, generation) {
+	// todo:  move this into generation domain obj
 	// detailed here:  http://boxcar2d.com/about.html
 
+	var selectRandomGenome = function (selectableGenomes){
+		// who knows why this does not return sometimes.  don't care.  evolution baby.
+		return util.selectAtScore(selectableGenomes, _.random(0, 100)) || genome.createRandom();
+	};
+	
 	var Roulette = function(genomeMater){
 		this.genomeMater = genomeMater;
 	};
 
-	Roulette.prototype = {
-		getRandomGenome: function(selectableGenomes){
-			// who knows why this does not return sometimes.  don't care.  evolution baby.
-			return util.selectAtScore(selectableGenomes, _.random(0, 100)) || genome.createRandom();
-		},
-		nextGeneration: function(genomes, generationSize){
+	Roulette.prototype = _.extend(Roulette.prototype, {
+		nextGeneration: function(generation){
 			// filter out negative scores
-			genomes = _.filter(genomes, function(genome){
+			var genomes = _.filter(generation.genomes, function(genome){
 				return genome.score > 0;
 			});
 
 			// create new random genomes to complete generation
-			for (var i = genomes.length; i < generationSize; i++){
+			for (var i = genomes.length; i < config.generationSize; i++){
 				var newGenome = genome.createRandom();
 				newGenome.score = 0.1;
 				genomes.push(newGenome);
 			}
 
-			var nextGeneration = [];
-			while(nextGeneration.length <= generationSize){
-				var parentOne = this.getRandomGenome(genomes);
+			var nextGeneration = generation.createNew();
+			while(nextGeneration.genomes.length <= config.generationSize){
+				var parentOne = selectRandomGenome(genomes);
 				var genomesWithoutParentOne = _.without(genomes, parentOne);
 
-				var parentTwo = this.getRandomGenome(genomesWithoutParentOne);
+				var parentTwo = selectRandomGenome(genomesWithoutParentOne);
 
 				var children = this.genomeMater.mate(parentOne, parentTwo);
 				children.one.mutate();
 				children.two.mutate();
-				nextGeneration.push(children.one);
-				nextGeneration.push(children.two);
+				nextGeneration.genomes.push(children.one);
+				nextGeneration.genomes.push(children.two);
 			}
 			return nextGeneration;
 		}
-	};
+	});
 
 	return Roulette;
 });
