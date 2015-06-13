e35ba0ee2024491027bec1cf028ebb4bd2067e53
Anthony Sennett <anthony.sennett@gmail.com>
2015-06-13 16:23:48 +0200

    new unit test for Evolver.run

--- tests/core/EvolverTest.js
+++ tests/core/EvolverTest.js
@@ -1,20 +1,5 @@
 define(['core/Evolver', 'environment/Simulator', 'evolution/Engine'], function (Evolver, Simulator, Engine) {
 	
-	var fakeGeneration = "fake generation";
-	var createEvolver = function(){
-		spyOn(Simulator.prototype, 'runGeneration');
-		spyOn(Engine.prototype, 'nextGeneration').and.returnValue(fakeGeneration);
-		this.evolver = new Evolver(Simulator.prototype, Engine.prototype);
-	};
-	
-	var exerciseRun = function(){
-		this.evolver.run();
-	};
-	
-	var assertGenerationReceivedFromEvolutionEngine = function(){
-		expect(Simulator.prototype.runGeneration).toHaveBeenCalledWith(fakeGeneration, jasmine.any(Function));
-	};
-	
 	describe('Evolver', function () {
 		describe('run', function(){
 			it('gets the generation from the evolution engine', function(){
@@ -22,6 +7,32 @@ define(['core/Evolver', 'environment/Simulator', 'evolution/Engine'], function (
 				exerciseRun.call(this);
 				assertGenerationReceivedFromEvolutionEngine.call(this);
 			});
+
+			it('runs the passed callback when running', function(){
+				createEvolver.call(this);
+				exerciseRun.call(this);
+				assertOnRunningCallbackCalled.call(this);
+			});
 		});
 	});
+	
+	var assertOnRunningCallbackCalled = function(){
+		expect(this.runFunctionSpy).toHaveBeenCalled();
+	};
+
+	var fakeGeneration = "fake generation";
+	var createEvolver = function(){
+		spyOn(Simulator.prototype, 'runGeneration');
+		spyOn(Engine.prototype, 'nextGeneration').and.returnValue(fakeGeneration);
+		this.evolver = new Evolver(Simulator.prototype, Engine.prototype);
+	};
+
+	var exerciseRun = function(){
+		this.runFunctionSpy = jasmine.createSpy('runFunction');
+		this.evolver.run(this.runFunctionSpy);
+	};
+
+	var assertGenerationReceivedFromEvolutionEngine = function(){
+		expect(Simulator.prototype.runGeneration).toHaveBeenCalledWith(fakeGeneration, jasmine.any(Function));
+	};
 });
