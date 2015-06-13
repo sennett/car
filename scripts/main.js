a0f14aac8e5dc8c827e9f7d3a1d8698a782c81d6
Anthony Sennett <anthony.sennett@gmail.com>
2015-06-13 16:08:04 +0200

    dark theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 define(['Spinner', 'require'], function(Spinner, require) {
-    var spinner = new Spinner().spin(document.getElementById('loadingTarget'));
+    var spinner = new Spinner({
+		color:'#cbcbcb'
+	}).spin(document.getElementById('loadingTarget'));
 	require(['productionBuildWrapper'], function(){
 		spinner.stop();
 	})
