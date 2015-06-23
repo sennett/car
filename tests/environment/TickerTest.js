4a7f49b4eb14dc41b05c7eb190c5c8e9e0dac601
Anthony Sennett <anthony.sennett@gmail.com>
2015-06-23 23:52:58 +0200

    fixing unit tests

--- tests/environment/TickerTest.js
+++ tests/environment/TickerTest.js
@@ -2,22 +2,22 @@ define(['environment/Ticker'], function (Ticker) {
 	describe('Ticker', function () {
 		beforeEach(function(){
 			this.timeoutMock = {some:"mock"};
-			this.timeoutProviderSpy = jasmine.createSpyObj('intervalProviderSpy', ['setTimeout', 'clearTimeout']);
-			this.timeoutProviderSpy.setTimeout.and.returnValue(this.timeoutMock);
+			this.timeoutProviderSpy = jasmine.createSpyObj('intervalProviderSpy', ['requestAnimationFrame', 'cancelAnimationFrame']);
+			this.timeoutProviderSpy.requestAnimationFrame.and.returnValue(this.timeoutMock);
 			this.interval = {a:'thing'};
 			this.ticker = new Ticker(this.timeoutProviderSpy, this.interval);
 		});
 		describe('run', function(){
 			it('creates timeout with interval from VisibleTicker.setTimeout', function(){
 				this.ticker.run(function(){});
-				expect(this.timeoutProviderSpy.setTimeout).toHaveBeenCalled();
+				expect(this.timeoutProviderSpy.requestAnimationFrame).toHaveBeenCalled();
 			});
 		});
 		describe('stop', function(){
 			it('clears the interval provider with the same timeout ID', function(){
 				this.ticker.run(function(){});
 				this.ticker.stop();
-				expect(this.timeoutProviderSpy.clearTimeout).toHaveBeenCalledWith(this.timeoutMock);
+				expect(this.timeoutProviderSpy.cancelAnimationFrame).toHaveBeenCalledWith(this.timeoutMock);
 			});
 		});
 		xdescribe('speedUp and slowDown', function(){
@@ -27,17 +27,17 @@ define(['environment/Ticker'], function (Ticker) {
 					this.ticker.speedUp();
 				});
 				it('clears the current timeout', function(){
-					expect(this.timeoutProviderSpy.clearTimeout).toHaveBeenCalledWith(this.timeoutMock);
+					expect(this.timeoutProviderSpy.cancelAnimationFrame).toHaveBeenCalledWith(this.timeoutMock);
 				});
 				it('makes a new timeout', function(){
-					expect(this.timeoutProviderSpy.setTimeout).toHaveBeenCalled();
+					expect(this.timeoutProviderSpy.requestAnimationFrame).toHaveBeenCalled();
 				});
 			});
 			describe('when stopped', function(){
 				it('does not touch interval provider', function(){
 					this.ticker.speedUp();
-					expect(this.timeoutProviderSpy.setTimeout).not.toHaveBeenCalled();
-					expect(this.timeoutProviderSpy.clearTimeout).not.toHaveBeenCalled();
+					expect(this.timeoutProviderSpy.requestAnimationFrame).not.toHaveBeenCalled();
+					expect(this.timeoutProviderSpy.cancelAnimationFrame).not.toHaveBeenCalled();
 				});
 			});
 		})
