9a56a1d0721b66644220cbf10d1dd87655642165
Anthony Sennett <anthony.sennett@gmail.com>
2015-07-25 18:47:12 +0200

    add car score

--- scripts/ui/CarGraph.js
+++ scripts/ui/CarGraph.js
@@ -1,5 +1,6 @@
-define(['underscore', 'ui/presenters/GenerationsPresenter', 'Highcharts', 'ui/utils'], function (_, GenerationsPresenter, Highcharts, utils) {
+define(['underscore', 'ui/presenters/GenerationsPresenter', 'Highcharts', 'ui/utils', 'core/appConfig'], function (_, GenerationsPresenter, Highcharts, utils, appConfig) {
 	return function CarGraph(generationUiService) {
+		var carPointsPerGeneration = 1 / appConfig.generationSize;
 		var node = document.getElementById('carGraph');
 		var chart = new Highcharts.Chart({
 			chart:{
@@ -25,10 +26,17 @@ define(['underscore', 'ui/presenters/GenerationsPresenter', 'Highcharts', 'ui/ut
 				name: 'Average score'
 			},{
 				name: 'High score'
+			},{
+				name: 'Car score',
+				type: 'line',
+				pointInterval: carPointsPerGeneration
 			}]
 		});
+		var carsAddedThisGeneration = 0;
+		var carIdToXPositionMap = {};
 		var averageScoreSeries = chart.series[0];
 		var highScoreSeries = chart.series[1];
+		var carScoreSeries = chart.series[2];
 		var findGenerationPoint = function (generationId, series) {
 			return _.find(series.data, function (point) {
 				return point.x == generationId;
@@ -37,21 +45,34 @@ define(['underscore', 'ui/presenters/GenerationsPresenter', 'Highcharts', 'ui/ut
 		
 		var publicInterface = {
 			onNewGeneration: function(generationId){
+				carsAddedThisGeneration = 0;
 				averageScoreSeries.addPoint([generationId, 0]);
 				highScoreSeries.addPoint([generationId, 0]);
 				chart.redraw();
 			},
 			onNewGenerationHighScore: function(generationId, highscore){
 				highscore = utils.roundScore(highscore);
-				findGenerationPoint(generationId, highScoreSeries).update(highscore, false);
+				
+				var generationPoint = findGenerationPoint(generationId, highScoreSeries);
+				generationPoint.update(highscore, false);
 			},
 			onNewGenerationAverageScore: function(generationId, score){
 				score = utils.roundScore(score);
 				findGenerationPoint(generationId, averageScoreSeries).update(score, false);
 			},
-			onNewCar: function(){},
-			onNewCarScore: function(){},
-			onCarSimulationComplete: function(){}
+			onNewCar: function(carId, generationId){
+				var xPosition = generationId + carsAddedThisGeneration * carPointsPerGeneration;
+				carIdToXPositionMap[carId] = xPosition;
+				carScoreSeries.addPoint([xPosition, 0]);
+				carsAddedThisGeneration++;
+			},
+			onNewCarScore: function(carId, score){
+				var xpoint = findGenerationPoint(carIdToXPositionMap[carId], carScoreSeries);
+				xpoint.update(score, false);
+			},
+			onCarSimulationComplete: function(){
+				chart.redraw();
+			}
 		};
 		
 		new GenerationsPresenter(publicInterface, generationUiService);
